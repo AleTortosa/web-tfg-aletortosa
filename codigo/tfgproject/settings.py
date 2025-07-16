5f589e24c9e9e961790dad66d0948d9f15358c00
atordo <atordo@alumni.uv.es>
2025-07-16 20:08:14 +0200

    pruebas cookies

--- codigo/tfgproject/settings.py
+++ codigo/tfgproject/settings.py
@@ -24,7 +24,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-!w=sg#2iurzefzk_w2d1g+2@%_=$t%ya4(xu&!t&6x&d@@d@b3'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG', 'False').lower() == 'true'
+DEBUG = True
+
 
 ALLOWED_HOSTS = ['web-tfg-aletortosa-production.up.railway.app', 'localhost', '127.0.0.1']
 CSRF_TRUSTED_ORIGINS = [
@@ -145,3 +146,6 @@ if not DEBUG:
     CSRF_COOKIE_SECURE = True
     SECURE_BROWSER_XSS_FILTER = True
     SECURE_CONTENT_TYPE_NOSNIFF = True
+else:
+    # Forzar HTTP en desarrollo
+    SECURE_SSL_REDIRECT = False
